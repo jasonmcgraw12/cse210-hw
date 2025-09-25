e3b29f07f568f16e2a9ccf25948555b51c1f2178
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2025-09-25 11:18:14 -0600

    Finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("what is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+    static void PromtUserBirthYear(out int birthYear)
+    {
+        Console.Write("What year were you born? ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int num)
+    {
+        int numSquared = num * num;
+        return numSquared;
+    }
+    static void DisplayResult(string name, int numSquared, int birthYear)
+    {
+        Console.WriteLine($"Hello {name} your favorite number squared is"
+        + $" {numSquared} and you will turn {2025 - birthYear} years old this year");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int birthYear = 0;
+        PromtUserBirthYear(out birthYear);
+        int numSquared = SquareNumber(favNumber);
+        DisplayResult(name, numSquared, birthYear);
     }
 }
