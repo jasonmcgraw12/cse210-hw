2f815538566c58c9bdf9c7600a96aa423cf8fc3d
jasonmcgraw12 <jasonmcgraw12@byui.edu>
2025-11-22 10:00:48 -0700

    Made a leveling system for goal software

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -10,6 +10,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        string[] badges = ["Bronze", "Silver", "Gold"];
+        int[] levelUpAt = [100,500];
+        int level = 0;
         string input;
         string fileName = "";
         int totalPoints = 0;
@@ -38,22 +41,18 @@ class Program
                     CompleteGoal();
                     break;
                 case "3":
-                    Console.WriteLine($"Your total points is currently {totalPoints}! (press enter to continue)");
+                    Console.WriteLine($"Your total points is currently at {totalPoints}! ");
+                    Console.Write($"Your rank is {badges[level]}. ");
+                    if (badges[level] != "Gold")
+                    {
+                        Console.Write($"in order to level up you need {levelUpAt[level]} total points. ");
+                    }
+                    else
+                    {
+                        Console.Write("You are at max level! ");
+                    }
+                    Console.WriteLine("(press enter to continue)");
                     Console.ReadLine();
-                    // bool isOnFirstLine = true;
-                    // string path = Environment.CurrentDirectory+"\\GoalFiles\\";
-                    // if (Environment.CurrentDirectory == AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.Length - 1))
-                    // {
-                    //     path = "..\\..\\..\\GoalFiles\\";
-                    // }
-                    // foreach (string line in File.ReadLines(path+fileName))
-                    // {
-                    //     if (isOnFirstLine)
-                    //     {
-                    //         totalPoints = int.Parse(line);
-                    //         isOnFirstLine = false;
-                    //     }
-                    // }
                     break;
                 case "4":
                     LoadGoalFile();
@@ -67,92 +66,85 @@ class Program
         void MakeGoal()
         {
             input = "";
-            while(input != "5" && input != "4" && input != "3" && input != "2" && input != "1")
+            Console.Clear();
+            Console.WriteLine(
+                """
+            What kind of goal do you want to create? (enter '4' to end)
+            1. Simple Goal
+            2. Eternal Goal
+            3. Numbered Goal
+            4. End
+            """);
+            input = Console.ReadLine();
+            bool iscomplete = false;
+            switch (input)
             {
-                Console.Clear();
-                Console.WriteLine(
-                    """
-                What kind of goal do you want to create? (enter '4' to end)
-                1. Simple Goal
-                2. Eternal Goal
-                3. Numbered Goal
-                4. End
-                """);
-                input = Console.ReadLine();
-                bool iscomplete = false;
-                switch (input)
-                {
-                    case "1":
-                        Console.WriteLine("What is the title of your goal?");
-                        string goalTitle = Console.ReadLine();
-                        Console.WriteLine("Descibe what the goal is.");
-                        string goalDescription = Console.ReadLine();
-                        Console.WriteLine("How many ponts should this goal give on completion?");
+                case "1":
+                    Console.WriteLine("What is the title of your goal?");
+                    string goalTitle = Console.ReadLine();
+                    Console.WriteLine("Descibe what the goal is.");
+                    string goalDescription = Console.ReadLine();
+                    Console.WriteLine("How many ponts should this goal give on completion?");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("That isn't a correct input, please type in a number instead.");
                         input = Console.ReadLine();
-                        while (!int.TryParse(input, out int number))
-                        {
-                            Console.WriteLine("That isn't a correct input, please type in a number instead.");
-                            input = Console.ReadLine();
-                        }
-                        int goalPoints = int.Parse(input);
-                        SimpleGoal simpleGoal = new SimpleGoal(iscomplete, goalPoints, goalTitle, goalDescription);
-                        goals.Add(simpleGoal);
-                        // add SimpleGoal1 to file save system
-                        break;
-                    case "2":
-                        Console.WriteLine("What is the title of your goal?");
-                        goalTitle = Console.ReadLine();
-                        Console.WriteLine("Descibe what the goal is.");
-                        goalDescription = Console.ReadLine();
-                        Console.WriteLine("How many ponts should this goal give on completion?");
+                    }
+                    int goalPoints = int.Parse(input);
+                    SimpleGoal simpleGoal = new SimpleGoal(iscomplete, goalPoints, goalTitle, goalDescription);
+                    goals.Add(simpleGoal);
+                    break;
+                case "2":
+                    Console.WriteLine("What is the title of your goal?");
+                    goalTitle = Console.ReadLine();
+                    Console.WriteLine("Descibe what the goal is.");
+                    goalDescription = Console.ReadLine();
+                    Console.WriteLine("How many ponts should this goal give on completion?");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("That isn't a correct input, please type in a number instead.");
                         input = Console.ReadLine();
-                        while (!int.TryParse(input, out int number))
-                        {
-                            Console.WriteLine("That isn't a correct input, please type in a number instead.");
-                            input = Console.ReadLine();
-                        }
-                        goalPoints = int.Parse(input);
-                        EternalGoal eternalGoal = new(iscomplete, goalPoints, goalTitle, goalDescription);
-                        goals.Add(eternalGoal);
-                        break;
-                    case "3":
-                        Console.WriteLine("What is the title of your goal?");
-                        goalTitle = Console.ReadLine();
-                        Console.WriteLine("Descibe what the goal is.");
-                        goalDescription = Console.ReadLine();
-                        Console.WriteLine("How many ponts should this goal give on completion?");
+                    }
+                    goalPoints = int.Parse(input);
+                    EternalGoal eternalGoal = new(iscomplete, goalPoints, goalTitle, goalDescription);
+                    goals.Add(eternalGoal);
+                    break;
+                case "3":
+                    Console.WriteLine("What is the title of your goal?");
+                    goalTitle = Console.ReadLine();
+                    Console.WriteLine("Descibe what the goal is.");
+                    goalDescription = Console.ReadLine();
+                    Console.WriteLine("How many ponts should this goal give on completion?");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("That isn't a correct input, please type in a number instead.");
                         input = Console.ReadLine();
-                        while (!int.TryParse(input, out int number))
-                        {
-                            Console.WriteLine("That isn't a correct input, please type in a number instead.");
-                            input = Console.ReadLine();
-                        }
-                        int completionPoints = int.Parse(input);
-                        Console.WriteLine("How many ponts should this goal give when completing a part of it?");
+                    }
+                    int completionPoints = int.Parse(input);
+                    Console.WriteLine("How many ponts should this goal give when completing a part of it?");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("That isn't a correct input, please type in a number instead.");
                         input = Console.ReadLine();
-                        while (!int.TryParse(input, out int number))
-                        {
-                            Console.WriteLine("That isn't a correct input, please type in a number instead.");
-                            input = Console.ReadLine();
-                        }
-                        goalPoints = int.Parse(input);
-                        Console.WriteLine("How many times should this goal be done before it's complete?");
+                    }
+                    goalPoints = int.Parse(input);
+                    Console.WriteLine("How many times should this goal be done before it's complete?");
+                    input = Console.ReadLine();
+                    while (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("That isn't a correct input, please type in a number instead.");
                         input = Console.ReadLine();
-                        while (!int.TryParse(input, out int number))
-                        {
-                            Console.WriteLine("That isn't a correct input, please type in a number instead.");
-                            input = Console.ReadLine();
-                        }
-                        int maxChecks = int.Parse(input);
-                        NumberedGoal numberedGoal = new(iscomplete, goalPoints, completionPoints, 0,  maxChecks, goalTitle, goalDescription);
-                        goals.Add(numberedGoal);
-                        break;
-                    case "4":
-                        Console.WriteLine("Going back...");
-                        break;
-                }
+                    }
+                    int maxChecks = int.Parse(input);
+                    NumberedGoal numberedGoal = new(iscomplete, goalPoints, completionPoints, 0,  maxChecks, goalTitle, goalDescription);
+                    goals.Add(numberedGoal);
+                    break;
             }
-            SaveGoals(fileName, totalPoints, goals);
+            SaveGoals(fileName, totalPoints, level, goals);
         }
 
         void CompleteGoal()
@@ -186,14 +178,23 @@ class Program
                 else
                 {
                     totalPoints += pointsRewarded;
-                    Console.WriteLine($"Your total points is now at {totalPoints}! (press enter to continue)");
+                    Console.Write($"Your total points is now at {totalPoints}! ");
+                    if (totalPoints >= levelUpAt[level])
+                    {
+                        if (badges[level] != badges[badges.Length-1])
+                        {
+                            level++;
+                            Console.Write($"You leveled up! Your rank is now {badges[level]}. ");
+                        }
+                        Console.WriteLine("(press enter to continue)");
+                    }
                     Console.ReadLine();
                 }
-                SaveGoals(fileName, totalPoints, goals);
+                SaveGoals(fileName, totalPoints, level, goals);
             }
         }
         
-        static void SaveGoals(string fileName, int totalPoints, List<SimpleGoal> goals)
+        static void SaveGoals(string fileName, int totalPoints, int level, List<SimpleGoal> goals)
         {
             string path = Environment.CurrentDirectory+"\\GoalFiles\\";
             if (Environment.CurrentDirectory == AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.Length - 1))
@@ -202,7 +203,7 @@ class Program
             }
             using (StreamWriter outputFile = new StreamWriter(path + fileName, append: false))
             {
-                outputFile.WriteLine(totalPoints);
+                outputFile.WriteLine(totalPoints+$"||{level}");
                 foreach (SimpleGoal goal in goals)
                 {
                     outputFile.WriteLine(goal.GetInfo());
@@ -219,7 +220,9 @@ class Program
             {
                 if (isOnFirstLine)
                 {
-                    totalPoints = int.Parse(line);
+                    string[] parts = line.Split("||");
+                    totalPoints = int.Parse(parts[0]);
+                    level = int.Parse(parts[1]);
                     isOnFirstLine = false;
                 }
                 else
@@ -234,13 +237,11 @@ class Program
                         SimpleGoal simpleGoal = new SimpleGoal(isComplete, goalPoints, goalTitle, goalDescription);
                         goals.Add(simpleGoal);
                         
-                        // add SimpleGoal1 to file save system
                     }
                     else if (parts[0] == "EternalGoal")
                     {
                         SimpleGoal simpleGoal = new EternalGoal(isComplete, goalPoints, goalTitle, goalDescription);
                         goals.Add(simpleGoal);
-                        // add SimpleGoal1 to file save system
                     }
                     else if (parts[0] == "NumberedGoal")
                     {
@@ -249,14 +250,12 @@ class Program
                         int maxChecks = int.Parse(parts[8]);
                         SimpleGoal simpleGoal = new NumberedGoal(isComplete, goalPoints, completionPoints, currentChecks, maxChecks, goalTitle, goalDescription);
                         goals.Add(simpleGoal);
-                        // add SimpleGoal1 to file save system
                     }
                     else{Console.WriteLine($"ERROR: {parts[0]} does not equal one of the given types");}
-                    // CHANGE search through the strings and make corisponding goals
                     lines.Add(line);
                 }
             }
-            SaveGoals(fileName, totalPoints, goals);
+            SaveGoals(fileName, totalPoints, level, goals);
         }
 
         void LoadGoalFile()
@@ -291,24 +290,9 @@ class Program
                     Console.WriteLine("What would you like to name your file?");
                     input = Console.ReadLine();
                     fileName = input;
-                    SaveGoals(input, totalPoints, goals);
+                    SaveGoals(fileName, totalPoints, level, goals);
                 }
             }           
         }
-        // Console.WriteLine("Hello Develop05 World!");
-        // SimpleGoal mySimpleGoal = new SimpleGoal(50,"Make goal","make a goal program");
-        // mySimpleGoal.Display();
-        // mySimpleGoal.Complete();
-        // mySimpleGoal.Display();
-
-        // EternalGoal myEternalGoal = new(10,"Get up in the morning","Wake up each morning with a positive attitude");
-        // myEternalGoal.Complete();
-        // myEternalGoal.Display();
-
-        // NumberedGoal myNumberedGoal = new(10,200,2,"work on project","work on this project for 15 minutes");
-        // myNumberedGoal.Display();
-        // myNumberedGoal.Complete();
-        // myNumberedGoal.Complete();
-        // myNumberedGoal.Display();
     }
 }
